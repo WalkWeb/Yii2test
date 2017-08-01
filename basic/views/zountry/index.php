e551d6052181993f2e8aa16fd2b65bd8b7edd5c4
Walk <walk@uremont.com>
2017-08-01 16:42:01 +0300

    Add CRUD Zontry

---
+++ basic/views/zountry/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ZountrySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Zountries';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="zountry-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Zountry', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'code',
+            'name',
+            'population',
+            'id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
